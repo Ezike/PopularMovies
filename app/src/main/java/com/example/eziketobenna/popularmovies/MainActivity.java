2344e05e4a3dfcb788af17db2cc1bf1b70f7e5f8
Ezike Tobenna <tobeydaniels@gmail.com>
2018-08-27 22:53:40 +0100

    added butterknife/picasso dependency and Recyclerview

--- app/src/main/java/com/example/eziketobenna/popularmovies/MainActivity.java
+++ app/src/main/java/com/example/eziketobenna/popularmovies/MainActivity.java
@@ -1,13 +1,27 @@
 package com.example.eziketobenna.popularmovies;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
 
+    @BindView(R.id.movieRecyclerView)
+    RecyclerView movieRecyclerView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ButterKnife.bind(this);
+
+        //set layout manager to the recycler view
+        GridLayoutManager mGridLayoutManager = new GridLayoutManager(this, 2);
+        movieRecyclerView.setLayoutManager(mGridLayoutManager);
+
     }
 }
